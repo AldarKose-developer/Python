c72a02d0cf55660a04ed2a1634e3232e7782bef5
AldarKose-developer <82699886+AldarKose-developer@users.noreply.github.com>
2021-09-16 13:17:04 +0600

    update

--- lesson.py
+++ lesson.py
@@ -1,27 +1,3 @@
-# def foo(list):
-#     new_list = []
-#     end_list = []
-#     for i in range (len(list)):
-#         for j in range(len(list)):
-#             if(i != j):
-#                 new_list[i]=''.join(list)
-                
-# def foo(list):
-#     index = -1  
-#     for j in range(len(list)):
-#         for i in range(len(list)):
-#             if(i != index):
-#                 print(list[i], end = '')
-#         if index == -1:
-#             print(" ")
-#         else:
-#             print(list[index] + ' ')
-#         index += 1
-        
-
-# a = ['a','b','c']
-# foo(a)
-
 from itertools import permutations
  
 def foo(list):
